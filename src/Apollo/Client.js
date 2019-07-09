7fd0f481e598e7694f7313d58cc4610534b2cb12
Kang.Yuchan <ikimaku0517@gmail.com>
2019-07-10 02:46:00 +0900

    Header Logic

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,9 +2,17 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-    uri: "http://localhost:4001/",
+    uri: "http://localhost:4001",
     clientState: {
         defaults,
         resolvers
+    },
+    request: async operation => {
+        const token = await localStorage.getItem('token');
+        operation.setContext({
+            headers: {
+                Authorization: token ? `Bearer ${token}` : ''
+            }
+        });
     }
 });
