1242ffb25147f8cbd2a9ce324961946b93494b74
Kang.Yuchan <ikimaku0517@gmail.com>
2019-07-13 16:33:14 +0900

    Search Screen UI

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -4,6 +4,7 @@ import SearchPresenter from "./SearchPresenter";
 import { useQuery } from "react-apollo-hooks";
 import { SEARCH } from "./SearchQueries";
 
+
  export default withRouter(({ location: { search } }) => {
   const term = search.split("=")[1];
   const { data, loading } = useQuery(SEARCH, {
@@ -12,5 +13,6 @@ import { SEARCH } from "./SearchQueries";
       term
     }
   });
+  
   return <SearchPresenter searchTerm={term} loading={loading} data={data} />;
 });
